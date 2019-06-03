8ea140c8c716ead3c5d3de0fa23ac67e56462008
Will Morris <william.morris117@gmail.com>
2019-06-03 14:30:03 -0600

    Model and tableview

---
+++ CKBulletinBoard/Message.swift
@@ -0,0 +1,47 @@
+//
+//  Message.swift
+//  CKBulletinBoard
+//
+//  Created by Will morris on 6/3/19.
+//  Copyright © 2019 devmtn. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct Constants {
+    
+    static let recordKey = "Message"
+    static let textKey = "text"
+    static let timestampKey = "timestamp"
+}
+
+class Message {
+    
+    var text: String
+    var timestamp: Date
+    var ckRecordID: CKRecord.ID
+    
+    
+    init(text: String, timestamp: Date, ckRecordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+        self.text = text
+        self.timestamp = timestamp
+        self.ckRecordID = ckRecordID
+    }
+    
+    convenience init?(ckRecord: CKRecord) {
+        guard let text = ckRecord[Constants.textKey] as? String,
+            let timestamp = ckRecord[Constants.timestampKey] as? Date
+            else { return nil }
+        
+        self.init(text: text, timestamp: timestamp, ckRecordID: ckRecord.recordID)
+    }
+}
+
+extension CKRecord {
+    convenience init(message: Message) {
+        self.init(recordType: Constants.recordKey, recordID: message.ckRecordID)
+        self.setValue(message.text, forKey: Constants.textKey)
+        self.setValue(message.timestamp, forKey: Constants.timestampKey)
+    }
+}
